5cad11a54a1aa7c9a3b12be87da7e72ac2dba41c
Anton Modaresi <amodaresi@archerpoint.com>
2023-10-27 02:20:30 +0000

    Changes to 1. Call Create Altria Log WS in BC 2. Allow SPG to call LS Commerce to Create Altria Log

---
+++ Server/WcfService/BusinessLayer/Loyalty/CustomLoyBLL.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LSOmni.Common.Util;
+using LSOmni.DataAccess.Interface.BOConnection;
+using LSRetail.Omni.DiscountEngine.DataModels;
+using LSRetail.Omni.Domain.DataModel.Base;
+using LSRetail.Omni.Domain.DataModel.Base.Retail;
+
+namespace LSOmni.BLL.Loyalty
+{
+    public class CustomLoyBLL : BaseLoyBLL
+    {
+        private ICustomLoyBO iBOCustomConnection = null;
+
+        protected ICustomLoyBO BOCustom
+        {
+            get
+            {
+                if (iBOCustomConnection == null)
+                    iBOCustomConnection = GetBORepository<ICustomLoyBO>(config.LSKey.Key, config.IsJson);
+                return iBOCustomConnection;
+            }
+        }
+
+        public CustomLoyBLL(BOConfiguration config, int timeoutInSeconds)
+            : base(config, timeoutInSeconds)
+        {
+        }
+
+        public CustomLoyBLL(BOConfiguration config) : base(config, 0)
+        {
+        }
+
+        #region Altria
+        public void AltriaLogEntryCreate(string storeId, string offerId, string cardId, int activityType, int channelType)
+        {
+            BOCustom.AltriaLogEntryCreate(storeId, offerId, cardId, activityType, channelType);
+        }
+        #endregion
+    }
+}
+
+
--- Server/WcfService/Service/Common/LSOmniBaseCustom.cs
+++ Server/WcfService/Service/Common/LSOmniBaseCustom.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 
 using LSOmni.BLL;
+using LSOmni.BLL.Loyalty;
+using LSOmni.Common.Util;
+using LSRetail.Omni.Domain.DataModel.Base.Retail;
 
 namespace LSOmni.Service
 {
@@ -11,5 +15,51 @@ namespace LSOmni.Service
             CustomBLL myBLL = new CustomBLL(config);
             return myBLL.MyCustomFunction(data);
         }
+
+        public virtual List<PublishedOffer> PublishedOffersGetByCardId2(string cardId, string itemId, string storeId)
+        {
+            if (cardId == null)
+                cardId = string.Empty;
+            if (itemId == null)
+                itemId = string.Empty;
+
+            Statistics stat = logger.StatisticStartMain(config, serverUri);
+
+            try
+            {
+                logger.Debug(config.LSKey.Key, "PublishedOffersGetByCardId2 was called");
+                logger.Debug(config.LSKey.Key, "itemId:{0} cardId:{1} storeId:{2}", itemId, cardId);
+
+                OfferBLL bll = new OfferBLL(config, clientTimeOutInSeconds);
+                CustomLoyBLL customLoyBll = new CustomLoyBLL(config, clientTimeOutInSeconds);
+
+                List<PublishedOffer> list = bll.PublishedOffersGet(cardId, itemId, string.Empty, stat);
+                foreach (PublishedOffer it in list)
+                {
+                    logger.Debug(config.LSKey.Key, "PublishedOffersGetByCardId2 about to call AltriaLogEntryCreate");
+                    customLoyBll.AltriaLogEntryCreate(storeId, it.Id, cardId, 2, 3);
+                    logger.Debug(config.LSKey.Key, "PublishedOffersGetByCardId2 returned from calling AltriaLogEntryCreate");
+                    foreach (ImageView iv in it.Images)
+                    {
+                        iv.StreamURL = GetImageStreamUrl(iv);
+                    }
+                    foreach (OfferDetails od in it.OfferDetails)
+                    {
+                        od.Image.StreamURL = GetImageStreamUrl(od.Image);
+                    }
+                }
+                return list;
+            }
+            catch (Exception ex)
+            {
+                HandleExceptions(ex, "itemId:{0} cardId:{1}", itemId, cardId);
+                return null; //never gets here
+            }
+            finally
+            {
+                logger.StatisticEndMain(stat);
+            }
+        }
+
     }
 }
